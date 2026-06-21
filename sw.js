76ac0ed4f6b14e51a230f6a589204eb32204d89f
trippypaneer <joshi.shubham1818@gmail.com>
2026-06-21 19:15:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
